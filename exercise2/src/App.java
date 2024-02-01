ceaa219f0d4720a243cfdd09e1746a6b50ea1145
Declan Naughton <djn5293@rit.edu>
2024-02-01 09:14:39 -0500

    Created new classes.

--- exercise2/src/App.java
+++ exercise2/src/App.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
+import java.io.FileReader;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        
     }
 }
---
+++ exercise2/src/Leaked.java
@@ -0,0 +1,16 @@
+public class Leaked {
+    private String name;
+    private String email;
+    private String city;
+    private String mac;
+    private String timestamp;
+    private String creditcard;
+    public Leaked(String _name, String _email, String _city, String _mac, String _timestamp, String _creditcard){
+        this.name = _name;
+        this.email = _email;
+        this.city = _city;
+        this.mac = _mac;
+        this.timestamp = _timestamp;
+        this.creditcard = _creditcard;
+    }
+}
