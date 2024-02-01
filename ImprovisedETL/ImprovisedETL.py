d96e241a5d08e69cb5280e66e0a099c9a6cd8d26
Declan Naughton <djn5293@rit.edu>
2024-02-01 14:50:12 -0500

    Finished Task.

--- ImprovisedETL/ImprovisedETL.py
+++ ImprovisedETL/ImprovisedETL.py
@@ -12,13 +12,18 @@ file = open('data.json')
 #get the object as a dictionary.
 data = json.load(file)
 
-for line in data:
-    ##The creditcard number
-    card = line["creditcard"]
-    #If there is a credit card, continue. Else, loop.
-    if(card):
-        csvline = line["name"] + ","+card
-        print(csvline)
+#create, and write into a new csv file.
+with open(today+'.csv', 'w', newline='') as csvf:
+    author = csv.writer(csvf)
+    for line in data:
+        ##The creditcard number
+        card = line["creditcard"]
+        #If there is a credit card, continue. Else, loop.
+        if(card):
+            ##csvline = line["name"] + ","+card
+            ##print(csvline)
+            author.writerow([line["name"],card])
+        
     
 
 file.close()
