9a743b85b523ace9e19de5c31517dd682d4e0967
Declan Naughton <djn5293@rit.edu>
2024-02-01 14:41:49 -0500

    Can retrieve data.

--- ImprovisedETL/ImprovisedETL.py
+++ ImprovisedETL/ImprovisedETL.py
@@ -1,2 +1,24 @@
-msg = "Hello, World!"
-print(msg)
+import json
+import csv
+from datetime import date
+
+today = date.today().strftime("%Y%m%d")
+
+print("Hello! Today's date is ",today)
+
+#Open the JSON file.
+file = open('data.json')
+
+#get the object as a dictionary.
+data = json.load(file)
+
+for line in data:
+    ##The creditcard number
+    card = line["creditcard"]
+    #If there is a credit card, continue. Else, loop.
+    if(card):
+        csvline = line["name"] + ","+card
+        print(csvline)
+    
+
+file.close()
--- ImprovisedETL/Readme.txt
+++ ImprovisedETL/Readme.txt
@@ -1,1 +1,10 @@
-You need to have Python 3.8.12
+OS: MacOS
+- Darwin x64 21.6.0
+
+You need to have Python 3.12.1
+- Download: https://www.python.org/ftp/python/3.12.1/python-3.12.1-macos11.pkg
+
+IDE: VS Code vVersion: 1.77.3 (Universal)
+
+Python Extension
+- ID: ms-python.python
